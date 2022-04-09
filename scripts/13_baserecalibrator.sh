bc037eaef0c78fe6cad02c1b9529440a0f03ca3a
Olaitan I. Awe <laitanawe@gmail.com>
2022-04-09 21:18:15 +0100

    Add new scripts

--- scripts/13_baserecalibrator.sh
+++ scripts/13_baserecalibrator.sh
@@ -8,19 +8,22 @@ echo "CPUs: $cpu"
 ram=32
 echo "RAM: $ram"
 
-human_reference_fasta="../../databases/human_gencode/GCA_000001405.29_GRCh38.p14_genomic_ref.fasta"
-#NCBI dbSNP
-#This site is problematic
-#knownsite_snps1="../../databases/human_gencode/snps_vcf_files/ncbi/known_sites_GCF_000001405.39.vcf"
-knownsite_snps1="../../databases/human_gencode/snps_vcf_files/ncbi/known_sites_GCF_000001405.25.vcf"
+human_reference_fasta="../../databases/human_gencode/ref_broad/resources_broad_hg38_v0_Homo_sapiens_assembly38.fasta"
 
-#Broad Institute
-knownsite_snps2="../../databases/human_gencode/snps_vcf_files/broad/resources_broad_hg38_v0_1000G_omni2.5.hg38.vcf.gz"
-knownsite_snps3="../../databases/human_gencode/snps_vcf_files/broad/resources_broad_hg38_v0_1000G_phase1.snps.high_confidence.hg38.vcf.gz"
-knownsite_snps4="../../databases/human_gencode/snps_vcf_files/broad/resources_broad_hg38_v0_Axiom_Exome_Plus.genotypes.all_populations.poly.hg38.vcf.gz"
-knownsite_snps5="../../databases/human_gencode/snps_vcf_files/broad/resources_broad_hg38_v0_Homo_sapiens_assembly38.known_indels.vcf.gz"
-knownsite_snps6="../../databases/human_gencode/snps_vcf_files/broad/resources_broad_hg38_v0_Mills_and_1000G_gold_standard.indels.hg38.vcf.gz"
-knownsite_snps7="../../databases/human_gencode/snps_vcf_files/broad/resources_broad_hg38_v0_hapmap_3.3.hg38.vcf.gz"
+#SNPs from Broad Institute
+#knownsite_snps1="../../databases/human_gencode/snps_vcf_files/broad/resources_broad_hg38_v0_Homo_sapiens_assembly38.dbsnp138.vcf"
+knownsite_snps1="../../databases/human_gencode/snps_vcf_files/broad/resources_broad_hg38_v0_Homo_sapiens_assembly38.known_indels.vcf.gz"
+#knownsite_snps3="../../databases/human_gencode/snps_vcf_files/broad/resources_broad_hg38_v0_1000G.phase3.integrated.sites_only.no_MATCHED_REV.hg38.vcf"
+#knownsite_snps4="../../databases/human_gencode/snps_vcf_files/broad/resources_broad_hg38_v0_Axiom_Exome_Plus.genotypes.all_populations.poly.hg38.vcf.gz"
+#knownsite_snps5="../../databases/human_gencode/snps_vcf_files/broad/resources_broad_hg38_v0_hapmap_3.3.hg38.vcf.gz"
+#knownsite_snps6="../../databases/human_gencode/snps_vcf_files/broad/resources_broad_hg38_v0_1000G_omni2.5.hg38.vcf.gz"
+#knownsite_snps7="../../databases/human_gencode/snps_vcf_files/broad/resources_broad_hg38_v0_1000G_phase1.snps.high_confidence.hg38.vcf.gz"
+#knownsite_snps8="../../databases/human_gencode/snps_vcf_files/broad/resources_broad_hg38_v0_Mills_and_1000G_gold_standard.indels.hg38.vcf.gz"
+
+#dbSNP from NCBI
+#The site .39.vcp is problematic for GRCh38 human genome build
+#knownsite_snps1="../../databases/human_gencode/snps_vcf_files/ncbi/known_sites_GCF_000001405.39.vcf"
+#knownsite_snps9="../../databases/human_gencode/snps_vcf_files/ncbi/known_sites_GCF_000001405.25.vcf"
 
 
 outdir_recal_data="../../data_99/recal_data_out/"
@@ -39,21 +42,26 @@ echo
 echo "Starting GATK BaseRecalibrator step ..."
 
 echo
+count_acc=0
 for input_bam in $input_sequence_pattern; do accession=$(echo $input_bam | sed 's/[^S]*//' | sed 's/.remdup.bam//');
-  echo "processing accession Id: " $accession \
+  ((count_acc++))
+  echo "processing accession Id "$count_acc": " $accession \
 
 ##BaseRecalibrator Step:
 gatk BaseRecalibrator \
    -I $outdir_remdup$accession.remdup.bam \
    -R $human_reference_fasta \
    --known-sites $knownsite_snps1 \
-   --known-sites $knownsite_snps2 \
-   --known-sites $knownsite_snps3 \
-   --known-sites $knownsite_snps4 \
-   --known-sites $knownsite_snps5 \
-   --known-sites $knownsite_snps6 \
-   --known-sites $knownsite_snps7 \
    -O $outdir_recal_data$recal_data_name
+
+#   --known-sites $knownsite_snps2 \
+#   --known-sites $knownsite_snps3 \
+#   --known-sites $knownsite_snps4 \
+#   --known-sites $knownsite_snps5 \
+#   --known-sites $knownsite_snps6 \
+#   --known-sites $knownsite_snps7 \
+#   --known-sites $knownsite_snps8 \
+#   --known-sites $knownsite_snps9 \
 
   echo;
   done
