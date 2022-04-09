da0fd63eeeed607979dd2390ac9d5b04168ad9f7
Olaitan I. Awe <laitanawe@gmail.com>
2022-04-09 12:33:08 +0100

    Add new scripts

--- scripts/12_snps_vcf_broad_load.sh
+++ scripts/12_snps_vcf_broad_load.sh
@@ -3,12 +3,12 @@
 cpus=24
 echo "CPUS: $cpus"
 
-base_url="https://ftp.ncbi.nih.gov/snp/latest_release/VCF/"
+base_url=""
 echo "BASE URL: $base_url"
 
-download_filename="../../databases/human_gencode/snps_vcf_files/snps_homosapiens_urls.txt"
+download_filename="../../databases/human_gencode/snps_vcf_files/broad/snps_homosapiens_urls2.txt"
 echo "Download Filename: $download_filename"
-outdir="../../databases/human_gencode/snps_vcf_files/"
+outdir="../../databases/human_gencode/snps_vcf_files/broad/"
 echo "Output Directory: $outdir"
 
 echo
@@ -17,20 +17,20 @@ echo "Checking to make sure that the output directory already exists ..."
 mkdir -pv $outdir
 
 echo
-for u in $(cat $download_filename); do wget -O $outdir$u $base_url$u; 
-#echo "WGET successfully processed: "$u; \
-#done
+for u in $(cat $download_filename); do wget -O $outdir$(basename $u) $u; 
+echo "WGET successfully processed: "$u; \
+done
 
 echo
 echo "WGET successfully downloaded all files!"
 
 echo
 #fastq -> fasta conversion
-for f in $(ls $outdir*.gz); do knownsite=$(echo $f | sed 's/[^GCF]*//' | sed 's/.gz//'); echo "Known site: "$knownsite".vcf"; 
-
+for f in $(ls $outdir*.gz); do fname=$(basename $f); knownsite=$(echo $f | sed 's/.gz//'); echo "Known site file: "$fname; 
+echo $f "is a .gz file"; \
 echo "Converting SNPs from .gz file to .vcf ..."; \
-
-zcat $f > $outdir"known_sites_"$knownsite".vcf";
+zcat $f  $outdir"known_sites_"$knownsite;\
+echo;
 done 
 
 echo "SNPs or known sites successfully generated in as VCF!"
---
+++ scripts/12_snps_vcf_dbsnp_ncbi_dload.sh
@@ -0,0 +1,38 @@
+#/usr/bin/env bash
+
+cpus=24
+echo "CPUS: $cpus"
+
+base_url="https://ftp.ncbi.nih.gov/snp/latest_release/VCF/"
+echo "BASE URL: $base_url"
+
+download_filename="../../databases/human_gencode/snps_vcf_files/snps_homosapiens_urls.txt"
+echo "Download Filename: $download_filename"
+outdir="../../databases/human_gencode/snps_vcf_files/ncbi/"
+echo "Output Directory: $outdir"
+
+echo
+echo "Checking to make sure that the output directory already exists ..."
+
+mkdir -pv $outdir
+
+echo
+for u in $(cat $download_filename); do wget -O $outdir$u $base_url$u; 
+echo "WGET successfully processed: "$u; \
+done
+
+echo
+echo "WGET successfully downloaded all files!"
+
+echo
+#fastq -> fasta conversion
+for f in $(ls $outdir*.gz); do knownsite=$(echo $f | sed 's/[^GCF]*//' | sed 's/.gz//'); echo "Known site: "$knownsite".vcf"; 
+echo $f "is a .gz file"; \
+echo "Converting SNPs from .gz file to .vcf ..."; \
+zcat $f > $outdir"known_sites_"$knownsite".vcf";
+done 
+
+echo "SNPs or known sites successfully generated in as VCF!"
+
+# Command Line
+# ./11_snps_vcf_download.sh
