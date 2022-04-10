9ef037e2fe6bc04695aaa705933745792425cb2e
Olaitan I. Awe <laitanawe@gmail.com>
2022-04-10 22:09:20 +0100

    Add new scripts

--- scripts/06_bwa_map.sh
+++ scripts/06_bwa_map.sh
@@ -1,31 +1,39 @@
-#!/usr/bin/env bash
+#/usr/bin/env bash
 
-# This script maps reads to a reference genome
-
-cpus=24
+cpus=32
 echo "CPUS: $cpus"
 
-path_to_index_and_prefix="../../databases/human_index_gencode/human_index"
+path_to_index_and_prefix="../../stubdata/humangenome/index_broad"
 echo "PREFIX: $path_to_index_and_prefix"
 
-forward_seq_pattern="trimmed_fastqs/*_1_val_1.fq"
+forward_seq_pattern="../../stubdata/trimmed_fastqs/*_1_val_1.fq"
 echo "FORWARD SEQUENCE PATTERN: $forward_seq_pattern"
 
-#bwa index -p $path_to_index_and_prefix $ref_genome
-#echo "BWA Indexing successfully completed for $ref_genome and built in "
+outdir_sams="../../stubdata/sams_bwa"
+echo "SAM File Output Directory: "$outdir_sams
 
-# prefix: human_index_gencode/human_index
-mkdir -pv 02_sams
-mkdir -pv 02_logs
+outdir_logs="../../stubdata/logs_bwa"
+echo "Log File Output Directory: "$outdir_logs
 
-for file in $(ls $forward_seq_pattern); do a=$(echo $file | sed 's/[^SRR]*//' | sed 's/_1_val_1.fq//');
-   echo "Accession ID: "$a; \
+mkdir -pv $outdir_sams
+mkdir -pv $outdir_logs
+
+
+#ref_genome="../../databases/human_gencode/GCA_000001405.29_GRCh38.p14_genomic.fna.gz"
+#echo "REFERENCE GENOME: $ref_genome="
+
+count_acc=0
+for file in $(ls $forward_seq_pattern); do filename=$(basename $file); a=$(echo $filename | sed 's/_1_val_1.fq//'); 
+   ((++count_acc))
+   echo; \
+   echo "Accession ID $count_acc:"$a; \
    echo "Output SAM file: "sams_bwa/$a.sam; \
    echo "Output Log file: "logs_bwa/$a.log; \
-   bwa mem -M -t $cpus -R "@RG:\tID:$a\tSM:$a" $path_to_index_and_prefix $file ${file/_1_val_1.fq/_2_val_2.fq} \
-   > sams_bwa/$a.sam 2> logs_bwa/$a.log
+   bwa mem -M -t $cpus -R "@RG\tID:$a\tSM:$a" $path_to_index_and_prefix $file ${file/_1_val_1.fq/_2_val_2.fq} \
+   > $outdir_sams/$a.sam 2> $outdir_logs/$a.log
 done
 
-# Indexing command that we ran
-# bwa index -p human_index_gencode/human_index human_gencode/GCA_000001405.29_GRCh38.p14_genomic.fna.gz
-# bwa mem -M -t 1 -R "@RG\tID:COL_${INDEX}\tSM:COL_${INDEX}" human_index_gencode/human_index \
+echo 
+echo "Mapping process completed for all Fastq files!"
+# Mapping command that we ran
+# ./06_bwa_map.sh
