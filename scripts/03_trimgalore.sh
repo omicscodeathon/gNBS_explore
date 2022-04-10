ed76e4a078930cef1d9c3af8180fe2127fd187f3
Olaitan I. Awe <laitanawe@gmail.com>
2022-04-10 21:41:14 +0100

    Add new scripts

--- scripts/03_trimgalore.sh
+++ scripts/03_trimgalore.sh
@@ -1,8 +1,7 @@
-#!/usr/bin/env bash
+#/usr/bin/env bash
 
 ##This script expects the forward sequence as mandatory input
 
-#maximum number of cores for trim_galore is 8
 cpus=8
 echo "CPUS: $cpus"
 
@@ -12,10 +11,28 @@ echo "Quality: $qual"
 min_read_length=50
 echo "Minimum Read Length: $min_read_length"
 
-input_file=$1
-echo "INPUT FILE: $input_file"
+input_forward_pattern="../../stubdata/fastqs/*_1.fastq"
+echo "INPUT PATTERN: $input_forward_pattern"
 
-outdir=$2
+trimmed_reads_outdir="../../stubdata/trimmed_fastqs"
 echo "Output Directory: $outdir"
 
-trim_galore --paired $input_file ${input_file/_1/_2} -q $qual --length $min_read_length --cores $cpus --fastqc -o $outdir
+trimmed_reports_dir="../../stubdata/fastqc_trimmed_report"
+echo "Trimmed Reports Directory: "$trimmed_reports_dir
+
+
+count_acc=0
+for input_file in $input_forward_pattern; do accession=$(echo basename $input_file | sed 's/_1.fastq//');
+  ((count_acc++))
+  echo "Trimming adapters from accession Id "$count_acc": " $accession \
+
+  trim_galore --paired $input_file ${input_file/_1/_2} -q $qual --length $min_read_length --cores $cpus --fastqc -o $trimmed_reads_outdir
+
+  done
+
+mkdir -pv $trimmed_reports_dir
+mv -fv $trimmed_reads_outdir/*.{txt,html,zip} $trimmed_reports_dir
+
+echo "Post-trimming Fastqc Reports have been moved to this location: "$trimmed_reports_dir
+echo
+echo "Trimming for all Fastq files completed successfully!"
