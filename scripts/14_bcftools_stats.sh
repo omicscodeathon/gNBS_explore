9f7c0d61ed6a9262d67d1ae49c531de281d0b3f1
Olaitan I. Awe <laitanawe@gmail.com>
2022-04-15 18:43:50 +0100

    Add new scripts

---
+++ scripts/14_bcftools_stats.sh
@@ -0,0 +1,28 @@
+#/usr/bin/env bash
+# This script uses BCFtools to generate the stats from VCF files
+
+cpus=32
+echo "CPUS: "$cpus
+
+echo
+echo "BCF tools stats generation step is ongoing now, $(date +%a) $(date +'%Y-%m-%d %H:%M:%S') ..."
+echo
+
+path_to_vcf="../../stubdata/variants/*.vcf.gz"
+outdir_variants="../../stubdata/variants/"
+bcfdir="vcfstats"
+
+mkdir -pv $outdir_variants$bcfdir
+
+echo "Pattern for input files: "$path_to_vcf
+
+count_acc=0
+for file in $(ls $path_to_vcf); do filename=$(basename $file); a=$(echo $filename | sed 's/.vcf.gz//'); 
+   ((++count_acc))
+   echo; \
+   echo "BCFtools is now generating stats for accession ID $count_acc:"$a; \
+   bcftools stats -c indels -c snps $file > $outdir_variants$bcfdir/$a"_vcfstats.txt" 2> $outdir_variants$bcfdir/$a"_vcfstats.log"
+done
+
+echo 
+echo "The stats for all VCF files have been generated now, $(date +%a) $(date +'%Y-%m-%d %H:%M:%S')" 
